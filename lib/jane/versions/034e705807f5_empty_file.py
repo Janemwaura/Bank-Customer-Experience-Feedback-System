86150dd5e19c96ce86ce733d365aced8eb73d958
Janemwaura <mwaura461@gmail.com>
2024-02-21 19:28:04 +0300

    first submission

---
+++ lib/jane/versions/034e705807f5_empty_file.py
@@ -0,0 +1,24 @@
+"""empty file
+
+Revision ID: 034e705807f5
+Revises: 
+Create Date: 2024-02-21 19:15:57.493610
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '034e705807f5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
